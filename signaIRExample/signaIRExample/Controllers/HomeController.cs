b09c603b97a51d5cbe8b901a17c0e8dea9e96554
esraracc <esra2460@hotmail.com>
2022-11-07 19:16:23 +0300

    Initial commit

---
+++ signaIRExample/signaIRExample/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using signaIRExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace signaIRExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IHubContext<SignalR> _hubContext;
+
+        public HomeController(ILogger<HomeController> logger, IHubContext<SignalR> hubContext)
+        {
+            _logger = logger;
+            _hubContext = hubContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index([FromBody] JsonElement json)
+        {
+            await _hubContext.Clients.All.SendAsync("LoadProducts");
+            return Json(json);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ signaIRExample/signaIRExample/Models/SignalR.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace signaIRExample.Models
+{
+    public class SignalR : Hub
+    {
+    }
+}
